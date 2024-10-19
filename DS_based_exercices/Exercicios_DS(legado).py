6c48a618f6c1a1e79d56b80abddb6923b0cef986
4rthurC4stro <168373388+4rthurC4stro@users.noreply.github.com>
2024-10-19 22:25:55 +0100

    Update Exercicios_DS(legado).py

--- DS_based_exercices/Exercicios_DS(legado).py
+++ DS_based_exercices/Exercicios_DS(legado).py
@@ -1,18 +1,3 @@
-# Exercise 1: Total Sales per Product
-# Calculate the total sales for each product category across all months.
-
-# Exercise 2: Monthly Sales Trend
-# Analyze the monthly sales trend for a specific product (e.g., "Eletrônicos"). Plot a graph showing sales per month.
-
-# Exercise 3: Top-Selling Month for Each Product
-# Identify the month where each product category had the highest sales.
-
-# Exercise 4: Product Sales Comparison
-# Compare the total sales of all products in each month. Which month had the highest overall sales?
-
-# Exercise 5: Sales Growth Rate
-# Calculate the month-over-month growth rate in sales for a given product category.​
-
 import pandas as pd
 import matplotlib.pyplot as plt
 data = pd.read_csv('dados_aleatorios.csv')
